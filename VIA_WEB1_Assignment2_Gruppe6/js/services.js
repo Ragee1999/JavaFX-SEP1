1041786d3f86ed30f095be8c58ff00c0d3bfe367
HiroshikunDK <36033304+HiroshikunDK@users.noreply.github.com>
2023-12-11 16:59:50 +0100

    udates

--- VIA_WEB1_Assignment2_Gruppe6/js/services.js
+++ VIA_WEB1_Assignment2_Gruppe6/js/services.js
@@ -1,11 +1,3 @@
 
-
-let data = "./project_data.json";
-
-readFile(data)
-
-function readFile(file) {
-    const reader = new FileReader();
-    const result = reader.target.result
-    console.log(result[0]);
-}
+import data from './data.json' assert { type: 'json' };
+console.log(data);
