920fc7ea9f2252e2484b128245676e742d22bd57
FREDERIK DIAMOND <unknownuser0709@proton.me>
2023-12-11 16:53:45 +0100

    portfolio updates

---
+++ VIA_WEB1_Assignment2_Gruppe6/js/filereader.js
@@ -0,0 +1,46 @@
+fetch('../project_data.json')
+    .then(response => response.json())
+    .then(allProjects => {
+        const ongoingProjects = allProjects.filter(project => project.completed === "False");
+
+        const projectsContainer = document.querySelector('.projects-container');
+        const tableBody = document.querySelector('.table tbody');
+
+        projectsContainer.innerHTML = '';
+        tableBody.innerHTML = '';
+
+        const createProjectDivHTML = (project, imagePath) => `
+            <div class="individual-project">
+                <img class="project-img" src="${imagePath}" alt="Project Image" />
+                <div class="textbox">
+                    <div>
+                        <div>Project name: ${project.projectName}</div>
+                        <div>Project type: ${project.projectType}</div>
+                        <div>Manhours used: ${project.hoursSpent}</div>
+                        <div>Est. price: ${project.price.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</div>
+                        <div>Est. timeline (months): ${project.timeline}</div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        const createTableRowHTML = (project, index) => `
+            <tr>
+                <td>${project.projectName}</td>
+                <td>${project.projectType}</td>
+                <td>${project.hoursSpent}</td>
+                <td>${project.price.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}</td>
+                <td>${project.timeline}</td>
+                </tr>
+        `;
+
+        ongoingProjects.slice(0, 3).forEach((project, index) => {
+            const imagePath = `Images/industrial${index + 1}cropped.png`;
+            projectsContainer.innerHTML += createProjectDivHTML(project, imagePath);
+        });
+
+        ongoingProjects.slice(3).forEach((project, index) => {
+            tableBody.innerHTML += createTableRowHTML(project, index + 3);
+        });
+    })
+    .catch(error => console.error('Error:', error));
